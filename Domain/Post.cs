e4fb1651b0604e833cdd43c8c7f2b57c5b18f836
nyquists <nyquists@gmatc.matc.edu>
2019-10-13 14:49:00 -0500

    Added Post Class again

--- Domain/Post.cs
+++ Domain/Post.cs
@@ -1,9 +1,8 @@
-using System;
-
 namespace Domain
 {
     public class Post
     {
+        
         public Guid Id { get; set; }
         public string Title { get; set; }
         public string Body { get; set; }
