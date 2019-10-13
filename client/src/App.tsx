5c76c5c73d1591e9a5048b81a6f4d032f14c56b9
nyquists <nyquists@gmatc.matc.edu>
2019-10-13 16:22:36 -0500

    Completed Activity 5

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
+import React from "react";
+import "./App.css";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
-    values: []
-  }
+    posts: []
+  };
 
   componentDidMount() {
-    axios.get('http://localhost:5000/api/values')
-    .then((response) => {
-      this.setState({
-        values: response.data
+    axios
+      .get("http://localhost:5000/api/posts")
+      .then(response => {
+        this.setState({
+          posts: response.data
+        });
       })
-    })
-      .catch((error) => {
+      .catch(error => {
         console.error(`Error fetching data: ${error}`);
-      })
+      });
   }
-  
+
   render() {
+    const { posts } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">BlogBox</header>
-          {this.state.values.map((value: any) => <div key={value}>{value}</div>)}
+        <main className="App-content">
+          {posts.map((post: any) => (
+            <div key={post.id}>
+              <h1>{post.title}</h1>
+              <p>{post.body}</p>
+            </div>
+          ))}
+        </main>
       </div>
     );
   }
